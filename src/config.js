0d7d689b2cef8f0839fe31d7962bbc92ea25be22
Ming Cheng <cmgrace@bu.edu>
2021-02-03 23:18:29 -0500

    pre-deploy database

--- src/config.js
+++ src/config.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://dunder_mifflin@localhost/amazon",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://dunder_mifflin@localhost/amazon",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://dunder_mifflin@localhost/amazon-test",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
 };
