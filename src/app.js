87f41f01f8e1d142076b19c205b12b91032a757a
Ming Cheng <cmgrace@bu.edu>
2021-03-15 11:20:27 -0400

    deploy

--- src/app.js
+++ src/app.js
@@ -14,9 +14,9 @@ const app = express();
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
-//app.use(helmet());
+app.use(helmet());
 app.use(cors({ origin: "https://amazon-client-cmgrace.vercel.app/" }));
-app.use(cors());
+//app.use(cors());
 
 app.use("/api", amazonRouter);
 
